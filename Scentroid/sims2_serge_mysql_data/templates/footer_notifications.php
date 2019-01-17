df2a3813811d3d786113b886d1fd89c36a46376c
Serge Ayissi <ayissi.serge@gmail.com>
2019-01-17 10:02:00 -0500

    Add files via upload

---
+++ Scentroid/sims2_serge_mysql_data/templates/footer_notifications.php
@@ -0,0 +1,19 @@
+</div>
+</div>
+
+<script src="/includes/javascript/doubletaptogo.js"></script>
+
+<script>
+	$( function()
+	{
+		$( '#sidebar li:has(ul)' ).doubleTapToGo();
+	});
+</script>
+<script src="/includes/javascript/back_to_top.js"></script>
+
+</body>
+</html>
+
+<?php
+ob_end_flush();
+?>
