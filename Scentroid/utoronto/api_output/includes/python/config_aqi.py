0ecbb99725f469bff4a90e1801ec5e8dcf6c731e
Serge Ayissi <ayissi.serge@gmail.com>
2019-04-18 09:22:15 -0400

    Create config_aqi.py

---
+++ Scentroid/utoronto/api_output/includes/python/config_aqi.py
@@ -0,0 +1,52 @@
+############################################## AQI CONFIG ###########################################
+
+############################# CO Gas
+# Good:
+co_min_good_ppm = 0                                      # Don't change the Zero value!
+co_max_good_ppm = 0
+
+# Moderate:
+co_min_moderate_ppm = co_max_good_ppm                    # Don't change this value!
+co_max_moderate_ppm = 0
+
+# Sensitive:
+co_min_sensitive_ppm = co_max_moderate_ppm               # Don't change this value!
+co_max_sensitive_ppm = 0
+
+# Unhealthy:
+co_min_unhealthy_ppm = co_max_sensitive_ppm              # Don't change this value!
+co_max_unhealthy_ppm = 0
+
+# Very Unhealthy:
+co_min_very_ppm = co_max_unhealthy_ppm                   # Don't change this value!
+co_max_very_ppm = 0
+
+# Hazardous:
+co_min_hazardous_ppm = co_max_very_ppm                   # Don't Change this value!
+co_max_hazardous_ppm = 0
+
+############################# NO2 Gas
+# Good:
+no2_min_good_ppm = 0                                     # Don't change the Zero value!
+no2_max_good_ppm = 0
+
+# Moderate:
+no2_min_moderate_ppm = no2_max_good_ppm                  # Don't change this value!
+no2_max_moderate_ppm = 0
+
+# Sensitive:
+no2_min_sensitive_ppm = no2_max_moderate_ppm             # Don't change this value!
+no2_max_sensitive_ppm = 0
+
+# Unhealthy:
+no2_min_unhealthy_ppm = no2_max_sensitive_ppm            # Don't change this value!
+no2_max_unhealthy_ppm = 0
+
+# Very Unhealthy:
+no2_min_very_ppm = no2_max_unhealthy_ppm                 # Don't change this value!
+no2_max_very_ppm = 0
+
+# Hazardous:
+no2_min_hazardous_ppm = no2_max_very_ppm                 # Don't change this value!
+no2_max_hazardous_ppm = 0
+
