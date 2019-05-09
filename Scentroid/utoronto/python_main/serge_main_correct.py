9b8358f84158cd46506722714264e172eb154f5d
Serge Ayissi <ayissi.serge@gmail.com>
2019-05-09 17:19:58 -0400

    Update serge_main_correct.py

--- Scentroid/utoronto/python_main/serge_main_correct.py
+++ Scentroid/utoronto/python_main/serge_main_correct.py
@@ -1,13 +1,15 @@
                    if len(wind_direction) == 0:
                            query_wdir = "0"
                    else:
-                           query_wdir= float(wind_direction) + 166
-                           if query_wdir >= 360:
-                                    query_wdir = query_wdir - 360
+                           adj_query_wdir= float(wind_direction) + 166
+                           if adj_query_wdir >= 360:
+                                    adj_query_wdir = adj_query_wdir - 360
+                           query_wdir = str(adj_query_wdir)
 
                    if len(vehicle_direction) == 0:
                            query_vdir = "0"
                    else:
-                           query_vdir= float(vehicle_direction) + 59
-                           if query_vdir >= 360:
-                                    query_vdir = query_vdir - 360
+                           adj_query_vdir= float(vehicle_direction) + 59
+                           if adj_query_vdir >= 360:
+                                    adj_query_vdir = adj_query_vdir - 360
+                           query_vdir = str(adj_query_vdir)
