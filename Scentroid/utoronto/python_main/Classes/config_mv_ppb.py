0eca9ce3e2da97ec5a5bef457a74bfd32789d4bf
Serge Ayissi <ayissi.serge@gmail.com>
2019-05-13 13:51:00 -0400

    Update config_mv_ppb.py

--- Scentroid/utoronto/python_main/Classes/config_mv_ppb.py
+++ Scentroid/utoronto/python_main/Classes/config_mv_ppb.py
@@ -20,30 +20,6 @@ o3_pt1_sensitivity = 0
 o3_pt1_min_detection = 0
 o3_pt1_max_detection = 0
 
-############################# PM1 Gas
-pm1_pt1_zero_offset = 0
-pm1_pt1_sensitivity = 0
-pm1_pt1_min_detection = 0
-pm1_pt1_max_detection = 0
-
-############################# PM2.5 Gas
-pm25_pt1_zero_offset = 0
-pm25_pt1_sensitivity = 0
-pm25_pt1_min_detection = 0
-pm25_pt1_max_detection = 0
-
-############################# PM4 Gas
-pm4_pt1_zero_offset = 0
-pm4_pt1_sensitivity = 0
-pm4_pt1_min_detection = 0
-pm4_pt1_max_detection = 0
-
-############################# PM10 Gas
-pm10_pt1_zero_offset = 0
-pm10_pt1_sensitivity = 0
-pm10_pt1_min_detection = 0
-pm10_pt1_max_detection = 0
-
 
 ################################################### PolluTracker 2
 ############################# CO Gas
@@ -63,27 +39,3 @@ o3_pt2_zero_offset = 0
 o3_pt2_sensitivity = 0
 o3_pt2_min_detection = 0
 o3_pt2_max_detection = 0
-
-############################# PM1 Gas
-pm1_pt2_zero_offset = 0
-pm1_pt2_sensitivity = 0
-pm1_pt2_min_detection = 0
-pm1_pt2_max_detection = 0
-
-############################# PM2.5 Gas
-pm25_pt2_zero_offset = 0
-pm25_pt2_sensitivity = 0
-pm25_pt2_min_detection = 0
-pm25_pt2_max_detection = 0
-
-############################# PM4 Gas
-pm4_pt2_zero_offset = 0
-pm4_pt2_sensitivity = 0
-pm4_pt2_min_detection = 0
-pm4_pt2_max_detection = 0
-
-############################# PM10 Gas
-pm10_pt2_zero_offset = 0
-pm10_pt2_sensitivity = 0
-pm10_pt2_min_detection = 0
-pm10_pt2_max_detection = 0
