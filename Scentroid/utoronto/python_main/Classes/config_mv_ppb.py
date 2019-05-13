e82933efa70b5ac07db619a6931c55eb82470e5f
Serge Ayissi <ayissi.serge@gmail.com>
2019-05-13 11:10:01 -0400

    Create config_mv_ppb.py

---
+++ Scentroid/utoronto/python_main/Classes/config_mv_ppb.py
@@ -0,0 +1,89 @@
+############################################## MV/PPB CONFIG ##########################################
+#Use this Config file edit values for accurate MV/PPM calculations per sensor
+
+################################################### PolluTracker 1
+############################# CO Gas
+co_pt1_zero_offset = 0
+co_pt1_sensitivity = 0
+co_pt1_min_detection = 0
+co_pt1_max_detection = 0
+
+############################# NO2 Gas
+no2_pt1_zero_offset = 0
+no2_pt1_sensitivity = 0
+no2_pt1_min_detection = 0
+no2_pt1_max_detection = 0
+
+############################# O3 Gas
+o3_pt1_zero_offset = 0
+o3_pt1_sensitivity = 0
+o3_pt1_min_detection = 0
+o3_pt1_max_detection = 0
+
+############################# PM1 Gas
+pm1_pt1_zero_offset = 0
+pm1_pt1_sensitivity = 0
+pm1_pt1_min_detection = 0
+pm1_pt1_max_detection = 0
+
+############################# PM2.5 Gas
+pm25_pt1_zero_offset = 0
+pm25_pt1_sensitivity = 0
+pm25_pt1_min_detection = 0
+pm25_pt1_max_detection = 0
+
+############################# PM4 Gas
+pm4_pt1_zero_offset = 0
+pm4_pt1_sensitivity = 0
+pm4_pt1_min_detection = 0
+pm4_pt1_max_detection = 0
+
+############################# PM10 Gas
+pm10_pt1_zero_offset = 0
+pm10_pt1_sensitivity = 0
+pm10_pt1_min_detection = 0
+pm10_pt1_max_detection = 0
+
+
+################################################### PolluTracker 2
+############################# CO Gas
+co_pt2_zero_offset = 0
+co_pt2_sensitivity = 0
+co_pt2_min_detection = 0
+co_pt2_max_detection = 0
+
+############################# NO2 Gas
+no2_pt2_zero_offset = 0
+no2_pt2_sensitivity = 0
+no2_pt2_min_detection = 0
+no2_pt2_max_detection = 0
+
+############################# O3 Gas
+o3_pt2_zero_offset = 0
+o3_pt2_sensitivity = 0
+o3_pt2_min_detection = 0
+o3_pt2_max_detection = 0
+
+############################# PM1 Gas
+pm1_pt2_zero_offset = 0
+pm1_pt2_sensitivity = 0
+pm1_pt2_min_detection = 0
+pm1_pt2_max_detection = 0
+
+############################# PM2.5 Gas
+pm25_pt2_zero_offset = 0
+pm25_pt2_sensitivity = 0
+pm25_pt2_min_detection = 0
+pm25_pt2_max_detection = 0
+
+############################# PM4 Gas
+pm4_pt2_zero_offset = 0
+pm4_pt2_sensitivity = 0
+pm4_pt2_min_detection = 0
+pm4_pt2_max_detection = 0
+
+############################# PM10 Gas
+pm10_pt2_zero_offset = 0
+pm10_pt2_sensitivity = 0
+pm10_pt2_min_detection = 0
+pm10_pt2_max_detection = 0
