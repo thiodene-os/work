3ee126a1d07fde3796d4aae0cc80782b032a3bf9
Serge Ayissi <ayissi.serge@gmail.com>
2019-08-06 08:34:18 -0400

    Add files via upload

---
+++ Scentroid/utoronto/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once('/var/www/html/api_sync/includes/php/common.php');
+require_once('/var/www/html/api_sync/includes/php/libs/lib_data.php');
+
+echo buildJSONDataOutput() ;
+
+?>
