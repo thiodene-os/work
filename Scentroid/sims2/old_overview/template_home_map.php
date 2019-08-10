6a14c2bae74a17939becf1a8a6e7f56608fcbe00
Serge Ayissi <ayissi.serge@gmail.com>
2019-08-10 18:57:31 -0400

    Add files via upload

---
+++ Scentroid/sims2/old_overview/template_home_map.php
@@ -0,0 +1,21 @@
+<!-- Index content -->
+<div class="loader"></div>
+<div class="input_fields">
+
+<?php
+echo $update_input_field ;
+?>
+
+</div>
+<div class="full_width_data">
+
+<div class="box">
+  <div id="box_container"></div>
+  <div id="chartContainer1" style="height: 370px; width: 100%; background-color: transparent;"></div>
+</div>
+
+<?php
+echo $right_side_data ;
+?>
+
+</div>
